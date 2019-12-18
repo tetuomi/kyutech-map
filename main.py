eedf963638e8ca77e0078caf5633f00c28aa2392
take-cantik <miki.takeru508@mail.kyutech.jp>
2019-12-18 16:43:41 +0900

    Varidate invalid filename

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, redirect, request, render_template, url_for, flash, session
 from PIL import Image
+from werkzeug import secure_filename
 import os, io
 import base64
 
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = os.path.join("file://", os.getcwd(), 'uploads')
+app.secret_key = 'ahiahi'
+
 
 def pla_num(number):
     if number == 0:
@@ -15,29 +18,37 @@ def pla_num(number):
     elif number == 55:
         return '食堂'
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        return redirect(url_for('predict'))
-    else:
-        return render_template('index.html')
-
+    return render_template('index.html')
 
 @app.route('/predict', methods=['GET','POST'])
 def predict():
-    img = request.files["file"]
-    image = Image.open(img)
-    buf = io.BytesIO()
-    image.save(buf, 'png')
-    qr_b64str = base64.b64encode(buf.getvalue()).decode("utf-8")
-    qr_b64data = "data:image/png;base64,{}".format(qr_b64str)
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(url_for('index'))
+        else:
+            img = request.files["file"]
+            filename = secure_filename(img.filename)
+            
+            if filename == '':
+                flash('ファイルがないお')
+                return redirect(url_for('index'))
+            else:
+                image = Image.open(img)
+                buf = io.BytesIO()
+                image.save(buf, 'png')
+                qr_b64str = base64.b64encode(buf.getvalue()).decode("utf-8")
+                qr_b64data = "data:image/png;base64,{}".format(qr_b64str)
 
-    number = 55
+                number = 55
 
-    return render_template('predict.html', img=qr_b64data, number=number, place=pla_num(number))
+                return render_template('predict.html', img=qr_b64data, number=number, place=pla_num(number))
 
+    else:
+        return redirect(url_for('index'))
 
 if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+    app.run(host='0.0.0.0')
 
 
